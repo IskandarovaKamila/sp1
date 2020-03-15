4f566bddfe756d1b285ebbe77665cf2f24ba96b0
Kamila <rina29820@gmail.com>
2020-03-15 00:42:22 -0700

    commit1

---
+++ script19.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+echo "Автор: Искандарова Камила"
+echo "Название программы: kmbsnkm"
+echo "Краткое описание: Работа с паролями"
+
+while [[ $otvet != "n" ]]
+do
+	echo "Введите имя пользователя:"
+	read name
+	echo "Выберите тип действия для данного пользователя:"
+	echo "1. Заблокировать."
+	echo "2. Разблокировать."
+	read action
+	if [[ $action = 1 ]]
+	then
+		sudo passwd -l $name
+	else
+		if [[ $action = 2 ]]
+		then
+			sudo passwd -u $name
+		else
+			echo "Попробуй заново."
+		fi
+	fi
+
+	echo "Хотите продолжить (y) или выйти (n)?"
+	read otvet
+	if [ $otvet != "y" ] && [ $otvet != "n" ]
+	then
+		echo "Ответ неверный. Прекращение работы."
+		exit 0
+	fi
+done 
+exit 0
